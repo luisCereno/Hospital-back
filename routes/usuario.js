e79b511313881e8b222eb2c9d306f422a692a4aa
Luis <luis.cereno.t@gmail.com>
2019-11-14 16:03:08 -0300

    Se crea borrar usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -152,4 +152,47 @@ app.put('/:id', (req, res) => {
 
 });
 
+//============================================================== 
+// Borrar usuario
+//============================================================== 
+app.delete('/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        if (err) {
+
+            return res.status(500).json({
+
+                ok: false,
+                mensaje: 'Error al borrar usuario',
+                errors: err
+
+            });
+            
+        }
+
+        if (!usuarioBorrado) {
+
+            return res.status(400).json({
+
+                ok: false,
+                mensaje: 'No existe un usuario con ese id',
+                errors: {message: 'No existe un usuario con ese id'}
+
+            });
+            
+        }
+
+        res.status(200).json({
+
+            ok: true,
+            usuario: usuarioBorrado
+    
+        });
+
+    })
+});
+
 module.exports = app;
